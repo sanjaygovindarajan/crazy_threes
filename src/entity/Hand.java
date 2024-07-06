ad16946cf7888c2fc3c0c65369c3e4aa78e25e2b
sophia de uria <sophia.de.uria@mail.utoronto.ca>
2024-07-06 12:38:29 -0400

    Wrote Hand Entity1

--- src/entity/Hand.java
+++ src/entity/Hand.java
@@ -3,19 +3,22 @@ package entity;
 import java.util.List;
 
 public class Hand extends CardCollection {
-    private List<Card> hand;
+    private List<Card> cardList;
 
-    public Hand(List<Card> hand){
+    public Hand(){
+
+    }
+    public Hand(List<Card> cardList){
     }
 
-    public Card playCard(List<Card> hand, int i){
-        Card playedCard = hand.get(i);
-        hand.remove(i);
+    public Card playCard(List<Card> cardList, int i){
+        Card playedCard = cardList.get(i);
+        cardList.remove(i);
         return playedCard;
     }
 
     public List<Card> viewCards(){
-        return hand;
+        return cardList;
     }
 
 }
