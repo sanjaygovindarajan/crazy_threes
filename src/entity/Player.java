720d43b239363c7e41a9f3ab06a39e8a260596b9
mohd-wxleed <mohstrong2005@gmail.com>
2024-07-08 17:30:41 -0400

    Added basic player methods and attributes

--- src/entity/Player.java
+++ src/entity/Player.java
@@ -1,5 +1,37 @@
 package entity;
 
 public class Player {
+    private Hand cards;
+    private String name;
+    private int numWins;
+    public Player(String name) {
+        this.name = name;
+        this.cards = new Hand();
+        this.numWins = 0;
+    } //constructor
+
+    public Hand getHand() {
+        return cards;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public boolean hasCard(Card card) {
+        for (Card c: cards.cardList) {
+            return c.equals(card);
+        }
+        return false;
+    }
+
+    public void drawCard(Deck d) {
+        cards.addCard(d.dealCard());
+    }
+
+    public boolean hasWin() {
+            return cards.cardList.isEmpty()
+    }
+
 
 }
