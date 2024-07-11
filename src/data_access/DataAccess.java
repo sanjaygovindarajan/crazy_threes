a7ce2c9c9ed83ad01ca2456f30fc7dce3d106ebe
sanjaygovindarajan <sfgovindarajan@gmail.com>
2024-07-11 19:31:17 -0400

    Add javadoc

--- src/data_access/DataAccess.java
+++ src/data_access/DataAccess.java
@@ -16,6 +16,12 @@ public class DataAccess implements DataAccessInterface {
     public DataAccess(String databaseFile) {
         this.databaseFile = new File(databaseFile);
     }
+
+    /**
+     * Loads a Game object from the database
+     * @param name The identifier by which the game is stored in the database
+     * @return A Game object corresponding to the Game stored in the database
+     */
     public Game loadGameByName(String name) {
         String db = Files.readString(Path.of(this.databaseFile.getPath()));
         String[] gamesArray = db.split(",,,");
@@ -24,17 +30,36 @@ public class DataAccess implements DataAccessInterface {
                 return readGame(game);
             }
         }
-        return new Game();
+        throw new IndexOutOfBoundsException();
     }
+
+    /**
+     * Saves a Game object in the database
+     * @param game The game to be saved in the database
+     * @param name The identifier of the game by which it can be accessed later from the database
+     */
     public void saveGame(Game game, String name){
         String gameStr = convertGame(game, name);
         String db = gameStr + ",,," + Files.readString(Path.of(this.databaseFile.getPath()));
         Files.writeString(Path.of(this.databaseFile.getPath()), db);
     }
 
+    /**
+     * Converts a Card object to a String
+     * @param card The Card object corresponding to the Card to be saved
+     * @return A String with the first character corresponding to the suit of the card and the subsequent characters
+     * corresponding to the number of the card
+     */
+
     private String convertCard(Card card){
         return Character.toString(card.getCurrentSuit()) + Integer.toString(card.getCardNum());
     }
+
+    /**
+     * Converts a CardCollection object to a String
+     * @param cardList A CardCollection object corresponding to the CardCollection to be saved
+     * @return The CardCollection in string format (that is, the Card objects in string format separated by a comma)
+     */
     private String convertCardCollection(CardCollection cardList){
         List<String> cardListStr = new ArrayList<>(cardList.getCardList().size());
         for(Card card : cardList.getCardList()){
@@ -42,9 +67,22 @@ public class DataAccess implements DataAccessInterface {
         }
         return String.join(",",cardListStr);
     }
+
+    /**
+     * Converts a Player object to a String
+     * @param player A Player object corresponding to the player to be saved
+     * @return The player in string format
+     */
     private String convertPlayer(Player player){
         return String.join(";",player.getName(),convertCardCollection(player.viewHand()));
     }
+
+    /**
+     * Converts a Game object to a String and gives the game a name.
+     * @param game A Game object corresponding to the game to be saved
+     * @param name What the game should be named in the database
+     * @return The game in string format, with the name
+     */
     private String convertGame(Game game, String name){
         List<String> playerList = new ArrayList<>(game.getPlayers().size());
         for(Player player : game.getPlayers()){
@@ -54,10 +92,22 @@ public class DataAccess implements DataAccessInterface {
         return String.join(":",name,convertCardCollection(game.getDeck()),convertCardCollection(game.getDiscard()),playerListStr,Integer.toString(game.getTurn()));
     }
 
+    /**
+     * Creates a new Card based on a String.
+     * @param cardString A two-character string where the first character refers to the suit and the subsequent
+     *                   characters refer to the number of the card
+     * @return A Card object with suit and number based on the input.
+     */
     private Card readCard(String cardString){
-        return new Card(cardString.charAt(1), cardString.charAt(0));
+
+        return new Card(Integer.parseInt(cardString.substring(1)), cardString.charAt(0));
     }
 
+    /**
+     * Creates a list of cards based on a String.
+     * @param cardString A string containing a list of string representations of cards separated by a comma
+     * @return A list of cards contained in the input string.
+     */
     private List<Card> readCardCollection(String cardString){
         List<String> cardListStr = Arrays.asList(cardString.split(","));
         List<Card> cardList = new ArrayList<>(cardListStr.size());
@@ -67,6 +117,11 @@ public class DataAccess implements DataAccessInterface {
         return cardList;
     }
 
+    /**
+     * Creates a new player based on a String.
+     * @param playerString A String in the proper format
+     * @return A Player object with attributes based on the input.
+     */
     private Player readPlayer(String playerString){
         String[] playerArray = playerString.split(";");
         String name = playerArray[0];
@@ -74,6 +129,11 @@ public class DataAccess implements DataAccessInterface {
         return new Player(name, hand); //Add once constructor is complete
     }
 
+    /**
+     * Creates a new game based on a String.
+     * @param gameString A String in the proper format
+     * @return A Game object with attributes based on the input
+     */
     private Game readGame(String gameString){
         String[] gameArray = gameString.split(":");
         Deck deck = new Deck(readCardCollection(gameArray[1]));
@@ -84,7 +144,7 @@ public class DataAccess implements DataAccessInterface {
             playerList.add(readPlayer(player));
         }
         int turn = Integer.parseInt(gameArray[4]);
-        return new Game(); //Add once constructor is complete
+        return new Game(deck, playerList, turn, discard); //Add once constructor is complete
     }
 
 
