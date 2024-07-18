e635e312758460a5859487413a4a23436d63f05b
Zihan Zhao <zzhao.zhao@mail.utoronto.ca>
2024-07-18 18:42:45 -0400

    Changed the implementation of cardList from ArrayList to Linkedlist

--- src/entity/Hand.java
+++ src/entity/Hand.java
@@ -1,16 +1,18 @@
 package entity;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 public class Hand extends CardCollection {
 
     public Hand(){
-        this.cardList = new ArrayList<Card>();
+        this.cardList = new LinkedList<Card>();
     }
 
     public Hand(List<Card> cardList){
-        this.cardList = (ArrayList<Card>) cardList;
+        this.cardList = new LinkedList<>();
+        cardList.addAll(cardList);
     }
 
     public Card playCard(int i) {
