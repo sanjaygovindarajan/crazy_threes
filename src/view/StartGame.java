776e827567eb4a7392a862567fdfa69b80573839
sophia de uria <sophia.de.uria@mail.utoronto.ca>
2024-07-18 18:38:06 -0400

    Started the startGame view class to have console output

---
+++ src/view/StartGame.java
@@ -0,0 +1,9 @@
+package view;
+
+import use_case.player_actions.NewGame;
+
+public class StartGame {
+    public static void main(String[] args) {
+        System.out.println("You are now playing the game Crazy Three's! /n");
+    }
+}
