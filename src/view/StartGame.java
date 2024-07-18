060b38d6822b7546b16bcc5af326157bf992be73
sophia de uria <sophia.de.uria@mail.utoronto.ca>
2024-07-18 18:48:51 -0400

    Start of construction of StartGame

--- src/view/StartGame.java
+++ src/view/StartGame.java
@@ -1,9 +1,12 @@
 package view;
 
+import entity.Game;
 import use_case.player_actions.NewGame;
 
 public class StartGame {
     public static void main(String[] args) {
         System.out.println("You are now playing the game Crazy Three's! /n");
+        NewGame.setNamesList();
+        NewGame.setPlayers();
     }
 }
