04682ae07c6d93c833e072dce5d2b5833a3b34fc
gretlbaghdadi <gretl.baghdadi@gmail.com>
2024-07-18 18:39:43 -0400

    removed game and player from handleDrawCard (shouldnt reference them)

--- src/use_case/player_actions/DrawCardInputBoundary.java
+++ src/use_case/player_actions/DrawCardInputBoundary.java
@@ -1,11 +1,12 @@
 package use_case.player_actions;
 
 import entity.Game;
+import entity.GameInterface;
 import entity.Player;
 import entity.MissingCardException;
 
 public interface DrawCardInputBoundary {
-    void handleDrawCard(Game game, Player player) throws MissingCardException;
+    void handleDrawCard() throws MissingCardException;
 }
 
 
