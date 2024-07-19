49ef5f694cc4af54f283af18f3586173899c7fb9
sophia de uria <sophia.de.uria@mail.utoronto.ca>
2024-07-19 19:07:00 -0400

    Construction of Shuffle Use Case

--- src/use_case/deck_actions/Shuffle.java
+++ src/use_case/deck_actions/Shuffle.java
@@ -1,4 +1,19 @@
 package use_case.deck_actions;
 
+import entity.Deck;
+
 public class Shuffle {
+    private final Deck deck;
+
+    public Shuffle(Deck deck) {
+        this.deck = deck;
+    }
+
+    public void shuffle(){
+        deck.shuffle();
+    }
+
+    public void dealCard(){
+        deck.dealCard();
+    }
 }
