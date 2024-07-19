b73e6e8e714b1f015a3989e8a9d5ec3a57ba2744
mohd-wxleed <mohstrong2005@gmail.com>
2024-07-19 17:30:10 -0400

    Finished Use Case draft

---
+++ src/use_case/game_actions/read_rules/ReadRulesInteractor.java
@@ -0,0 +1,21 @@
+package use_case.game_actions.read_rules;
+
+import interface_adapter.ReadRulesOutputBoundary;
+
+public class ReadRulesInteractor implements ReadRulesInputBoundary {
+    final ReadRulesOutputBoundary presenter;
+
+    public ReadRulesInteractor(ReadRulesOutputBoundary presenter) {
+        this.presenter = presenter;
+
+    }
+
+    /**
+     * Displays rules based on a fixed string.
+     */
+    @Override
+    public void execute(){
+        presenter.prepareSuccessView("The rules are as follows:" + ""); //type out rules here (in empty string)
+    }
+
+}
