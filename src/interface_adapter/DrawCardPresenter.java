9fd21ed94370ec615ca1584055a4cf1d7fd0072f
gretlbaghdadi <gretl.baghdadi@gmail.com>
2024-07-20 10:31:21 -0400

    added javadoc

--- src/interface_adapter/DrawCardPresenter.java
+++ src/interface_adapter/DrawCardPresenter.java
@@ -3,10 +3,21 @@ package interface_adapter;
 import use_case.player_actions.DrawCardOutputBoundary;
 import use_case.player_actions.DrawCardOutputData;
 
+/**
+ * Formats and presents the result of a draw card operation.
+ * Implements the DrawCardOutputBoundary interface to handle the presentation of the
+ * draw card output data.
+ */
 public class DrawCardPresenter implements DrawCardOutputBoundary {
+    /**
+     * Presents the result of the draw card operation.
+     * Formats the output data and prints it to the console.
+     *
+     * @param outputData the output data containing the result of the draw card operation
+     */
     @Override
     public void presentDrawCard(DrawCardOutputData outputData) {
-        // Format the response model for the view
+        // Format the output data for the view
         System.out.println("Message: " + outputData.getMessage());
         System.out.println("Success: " + outputData.isSuccess());
     }
