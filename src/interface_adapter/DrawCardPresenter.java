bfa9f3663bdad99d5e2966cfd71d8825c94d729c
gretlbaghdadi <gretl.baghdadi@gmail.com>
2024-07-20 09:33:42 -0400

    defined DrawCardPresenter, implement DrawCardOutputBoundary + format data for the view

--- src/interface_adapter/DrawCardPresenter.java
+++ src/interface_adapter/DrawCardPresenter.java
@@ -1,5 +1,13 @@
 package interface_adapter;
 
-public class DrawCardPresenter implements DrawCardOutputBoundary{
+import use_case.player_actions.DrawCardOutputBoundary;
+import use_case.player_actions.DrawCardResponseModel;
 
+public class DrawCardPresenter implements DrawCardOutputBoundary {
+    @Override
+    public void presentDrawCard(DrawCardResponseModel responseModel) {
+        // Format the response model for the view
+        System.out.println("Message: " + responseModel.getMessage());
+        System.out.println("Success: " + responseModel.isSuccess());
+    }
 }
