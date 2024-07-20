146104109cfe542ec047f3273f71d1025953dd3d
gretlbaghdadi <gretl.baghdadi@gmail.com>
2024-07-20 10:32:31 -0400

    added javadoc

--- src/use_case/player_actions/DrawCardInteractor.java
+++ src/use_case/player_actions/DrawCardInteractor.java
@@ -2,10 +2,31 @@ package use_case.player_actions;
 
 import entity.*;
 
+/**
+ * Handles the logic for drawing a card in the game.
+ * This interactor checks if the current player has a playable card and, if not, draws a card
+ * from the deck until a playable card is found. It then interacts with the game state to
+ * play the card.
+ */
+
 public class DrawCardInteractor implements DrawCardInputBoundary {
+    /**
+     * Constructs a new DrawCardInteractor with the specified game and output boundary.
+     *
+     * @param game the game instance to interact with
+     * @param outputBoundary the boundary used to present results
+     */
 Game game;
 OutputBoundary outputBoundary;
 
+    /**
+     * Handles the process of drawing a card for the current player. If the player does not
+     * have a playable card, the method draws cards from the deck until a playable card is found.
+     * It then prepares a response model and passes it to the output boundary for presentation.
+     *
+     * @throws MissingCardException if there are no cards left in the deck to draw
+     */
+    @Override
     public void handleDrawCard() throws MissingCardException {
         boolean hasPlayableCard = false;
         Player player = game.getCurrentPlayer();
