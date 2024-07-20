a4cfe8f4f77211c2df0d564beb381168b6099239
gretlbaghdadi <gretl.baghdadi@gmail.com>
2024-07-20 09:22:27 -0400

    create the DrawCardController

--- src/interface_adapter/DrawCardController.java
+++ src/interface_adapter/DrawCardController.java
@@ -2,4 +2,13 @@ package interface_adapter;
 import use_case.player_actions.DrawCardInputBoundary;
 
 public class DrawCardController {
+    final DrawCardInputBoundary drawCardInteractor;
+
+    public DrawCardController(DrawCardInputBoundary drawCardInteractor) {
+        this.drawCardInteractor = drawCardInteractor;
+    }
+
+    public void drawCard() throws MissingCardException {
+        drawCardInteractor.handleDrawCard();
+    }
 }
