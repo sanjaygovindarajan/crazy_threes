7be1cea98ed461b81c64fc9135d3c51d65b5328e
gretlbaghdadi <gretl.baghdadi@gmail.com>
2024-07-20 09:45:24 -0400

    changed the name of responseModel to OutputData

--- src/interface_adapter/DrawCardPresenter.java
+++ src/interface_adapter/DrawCardPresenter.java
@@ -1,13 +1,13 @@
 package interface_adapter;
 
 import use_case.player_actions.DrawCardOutputBoundary;
-import use_case.player_actions.DrawCardResponseModel;
+import use_case.player_actions.DrawCardOutputData;
 
 public class DrawCardPresenter implements DrawCardOutputBoundary {
     @Override
-    public void presentDrawCard(DrawCardResponseModel responseModel) {
+    public void presentDrawCard(DrawCardOutputData outputData) {
         // Format the response model for the view
-        System.out.println("Message: " + responseModel.getMessage());
-        System.out.println("Success: " + responseModel.isSuccess());
+        System.out.println("Message: " + outputData.getMessage());
+        System.out.println("Success: " + outputData.isSuccess());
     }
 }
