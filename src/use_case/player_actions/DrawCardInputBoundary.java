8fa428eeb6c548abc0a9ab3d584dc988bfd0903a
gretlbaghdadi <gretl.baghdadi@gmail.com>
2024-07-20 10:24:21 -0400

    added javadoc

--- src/use_case/player_actions/DrawCardInputBoundary.java
+++ src/use_case/player_actions/DrawCardInputBoundary.java
@@ -2,7 +2,19 @@ package use_case.player_actions;
 
 import entity.MissingCardException;
 
+/**
+ * Input boundary interface for handling draw card operations.
+ * Defines the method required to perform the draw card action.
+ */
 public interface DrawCardInputBoundary {
+    /**
+     * Handles the logic for drawing a card for current player.
+     * Checks if the player has a playable card; if not, draws cards from the deck until
+     * a playable card is found. This method may throw an exception if there are no cards left
+     * in the deck.
+     *
+     * @throws MissingCardException if there are no cards left in the deck to draw
+     */
     void handleDrawCard() throws MissingCardException;
 }
 
