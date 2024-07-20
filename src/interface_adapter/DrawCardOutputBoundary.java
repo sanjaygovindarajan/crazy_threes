ba58ff911d18fd5cf07f4ccff7095e61f08b96fb
gretlbaghdadi <gretl.baghdadi@gmail.com>
2024-07-20 10:30:11 -0400

    removed the extra shell (idk why it was there) added javadoc fixed responseModel --> outputdata (i missed it from last pull request)

--- src/interface_adapter/DrawCardOutputBoundary.java
+++ src/interface_adapter/DrawCardOutputBoundary.java
@@ -1,9 +1,17 @@
 package interface_adapter;
 package use_case.player_actions;
 
+/**
+ * Output boundary interface for presenting draw card operation results.
+ * Defines the method required to present the outcome of the draw card operation to the view.
+ */
 public interface DrawCardOutputBoundary {
-    public interface DrawCardOutputBoundary {
-        void presentDrawCard(DrawCardResponseModel responseModel);
+    /**
+     * Presents the result of the draw card operation.
+     *
+     * @param responseModel the response model containing the result of the operation
+     */
+        void presentDrawCard(DrawCardOutputData outputData);
     }
 }
 
