05ca8b7d819287a6fb2715430d147074f0c1bb8f
sophia de uria <sophia.de.uria@mail.utoronto.ca>
2024-07-20 08:47:18 -0400

    Construction of Shuffle Use Case + its input boundary

--- src/use_case/deck_actions/Shuffle.java
+++ src/use_case/deck_actions/Shuffle.java
@@ -2,13 +2,14 @@ package use_case.deck_actions;
 
 import entity.Deck;
 
-public class Shuffle {
+public class Shuffle implements ShuffleInputBoundary {
     private final Deck deck;
 
     public Shuffle(Deck deck) {
         this.deck = deck;
     }
 
+    @Override
     public void shuffle(){
         deck.shuffle();
         deck.dealCard();
