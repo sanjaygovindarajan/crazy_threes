95c9195968d33c4e363acaad399e55296b381458
gretlbaghdadi <gretl.baghdadi@gmail.com>
2024-07-20 09:25:27 -0400

    Define the DrawCardOutputBoundary interface

--- src/interface_adapter/DrawCardOutputBoundary.java
+++ src/interface_adapter/DrawCardOutputBoundary.java
@@ -1,4 +1,11 @@
 package interface_adapter;
+package use_case.player_actions;
 
 public interface DrawCardOutputBoundary {
+    public interface DrawCardOutputBoundary {
+        void presentDrawCard(DrawCardResponseModel responseModel);
+    }
 }
+
+
+
