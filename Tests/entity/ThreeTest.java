4809147cbaddaf9813b4d39479ae885df12f17a1
gretlbaghdadi <gretl.baghdadi@gmail.com>
2024-07-21 12:31:16 -0400

    tests getNewSuit and getCurrentSuit

--- Tests/entity/ThreeTest.java
+++ Tests/entity/ThreeTest.java
@@ -8,9 +8,19 @@ class ThreeTest {
 
     @Test
     void setNewSuit() {
+        Three three = new Three('s');
+        assertEquals('s', three.getCurrentSuit());
+
+        three.setNewSuit('h'); // Changing suit to Hearts
+        assertEquals('h', three.getCurrentSuit());
     }
 
     @Test
     void getCurrentSuit() {
+        Three three = new Three('c');
+        assertEquals('c', three.getCurrentSuit());
+
+        three.setNewSuit('d');
+        assertEquals('d', three.getCurrentSuit());
     }
 }
