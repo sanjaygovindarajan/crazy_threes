c4602f225ce6e2991314885750e0cfd38c6969d7
gretlbaghdadi <gretl.baghdadi@gmail.com>
2024-07-21 12:35:35 -0400

    added brief javadoc for tests

--- Tests/entity/ThreeTest.java
+++ Tests/entity/ThreeTest.java
@@ -6,15 +6,22 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class ThreeTest {
 
+    /**
+     * Verifies that the suit of the card can be changed and retrieved correctly.
+     */
+    @Test
     @Test
     void setNewSuit() {
         Three three = new Three('s');
         assertEquals('s', three.getCurrentSuit());
 
-        three.setNewSuit('h'); // Changing suit to Hearts
+        three.setNewSuit('h');
         assertEquals('h', three.getCurrentSuit());
     }
 
+    /**
+     * Verifies that the initial suit of the card is set correctly and can be retrieved.
+     */
     @Test
     void getCurrentSuit() {
         Three three = new Three('c');
