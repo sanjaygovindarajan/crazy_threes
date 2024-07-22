45be06f56513db9eee14a16290e45307373b009f
zoeparas <zoe.paraskevopoulos@mail.utoronto.ca>
2024-07-21 20:48:51 -0400

    Java doc and

--- src/use_case/player_actions/play_card/PlayCardInputBoundary.java
+++ src/use_case/player_actions/play_card/PlayCardInputBoundary.java
@@ -4,10 +4,25 @@ import entity.GameInterface;
 import interface_adapter.start_game.StartGameOutputBoundary;
 
 public interface PlayCardInputBoundary {
+
+    /**
+     * Allows for a card to be played in the game.
+     * @param number the number of the card to be played.
+     * @param suit the suit of the card to be played.
+     */
     void playCard(int number, char suit);
 
+    /**
+     * Sets the current game being played.
+     * @param game The game currently being played.
+     */
     void setGame(GameInterface game);
 
+    /**
+     * Allows for a three to be played in the game and for the suit of the disposal deck to be set.
+     * @param suit the suit of the card being played.
+     * @param newSuit the suit the disposal deck will begin accepting.
+     */
     void playThree(char suit, char newSuit);
 
     StartGameOutputBoundary getPresenter();
