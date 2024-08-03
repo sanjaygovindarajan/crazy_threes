f061d43f87e6b15f6db69a194a9ac029a85c78e8
sophia de uria <sophia.de.uria@mail.utoronto.ca>
2024-08-03 19:58:24 -0400

    Change of ShufflePresenter using JOptionPane for Phase 2

--- src/interface_adapter/shuffle/ShufflePresenter.java
+++ src/interface_adapter/shuffle/ShufflePresenter.java
@@ -1,22 +1,28 @@
 package interface_adapter.shuffle;
 
-
 import interface_adapter.ViewManagerModel;
+
+import javax.swing.*;
 
 /**
  * Formats and presents the result of shuffling a deck operation.
  * Implements the ShuffleOutputBoundary interface to handle the shuffling
  */
 public class ShufflePresenter implements ShuffleOutputBoundary {
+    private ViewManagerModel viewManagerModel;
+
     public ShufflePresenter(ViewManagerModel viewManagerModel) {
     }
 
     /**
      * Presents the result of the shuffle operation.
-     * Formats the output data and prints it to the console.
-     * Constructor for phase 1.
+     *  Constructor for phase 1: Formats the output data and prints it to the console.
+     *
+     * Now modified for phase 2: JOptionPane that outputs a message.
+     *
      */
     public void loadSuccessful(){
-        System.out.println("Shuffle done successfully");
+        JOptionPane.showMessageDialog(null, "Shuffled successfully");
+        this.viewManagerModel.setActiveView("Turn view");
     }
 }
