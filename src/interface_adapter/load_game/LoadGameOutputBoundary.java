557955dc642d3ccbe7b91bbdc921e186a65d8bc1
Zihan Zhao <zzhao.zhao@mail.utoronto.ca>
2024-08-08 15:38:35 -0400

    Add Javadoc to view game and load game.

--- src/interface_adapter/load_game/LoadGameOutputBoundary.java
+++ src/interface_adapter/load_game/LoadGameOutputBoundary.java
@@ -3,6 +3,9 @@ package interface_adapter.load_game;
 import interface_adapter.start_game.StartGameOutputBoundary;
 
 public interface LoadGameOutputBoundary extends StartGameOutputBoundary {
-
+    /**
+     * Prints an error message.
+     * @param error The error message.
+     */
     void prepareFailView(String error);
 }
